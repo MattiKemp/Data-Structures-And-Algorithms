defbd99061af7b7a14cc19392992a8e19e1deb19
Server <mkemp4@uiowa.edu>
2020-10-20 23:25:33 -0500

    added-doubly-ll

--- Common/Python/LinkedList.py
+++ Common/Python/LinkedList.py
@@ -1,8 +1,35 @@
-class SinglyLL():
+class SinglyLLNode():
     def __init__(self,val=None,next=None):
         self.val = val
         self.next = next
 
-def main():
+class DoublyLLNode():
+    def __init__(self,val=None,prev=None,next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class SinglyLL():
+    def __init__(self, root=None):
+        self.root = first
+
+class DoublyLL():
+    def __init__(self, root=None, end=None):
+        self.root = root
+        self.end = end
+
+def  main():
     print("Singly Linked List")
-main()
+
+if __name__ == "__main__":
+    main()
+
+#Notes:
+# Linked List are common implemented either as just nodes or as a container class 
+# to assist in ease of use such as looping/keeping track of the first node.
+
+# Linked List are commonly used to implement stack and queue data structures.
+
+# Common tasks to be done on LL are: Sorting, Reversing, Checking for Loops, and Inserting/Removing.
+
+
