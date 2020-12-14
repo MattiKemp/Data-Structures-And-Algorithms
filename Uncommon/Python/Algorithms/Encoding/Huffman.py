1b16143eabe81b14c3d87b1700047d97d1e02b75
Server <mkemp4@uiowa.edu>
2020-12-13 22:08:55 -0600

    huffman tree basic setup

---
+++ Uncommon/Python/Algorithms/Encoding/Huffman.py
@@ -0,0 +1,17 @@
+from LL import Queue
+#generates a Huffman coding tree from a list of symbols, s, sorted by ascending frequency 
+#and a list of the corresponding frequencies, f.
+#returns the root of the Huffman tree
+#runtime: O(s), space complexity: O(s)
+def generateTree(s,f):
+    print('temp')   
+
+
+def main():
+    print('main')
+    Queue()
+
+
+if __name__ == "__main__":
+    main()
+
