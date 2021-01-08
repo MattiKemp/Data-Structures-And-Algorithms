9ee3dc0168b5279e1ca65cc22918bd7c69f26d09
root <root@DESKTOP-68UL55P.localdomain>
2021-01-08 04:41:26 -0600

    Dynamic List in java

---
+++ Common/Python/Algorithms/LLAlgos/editing-reversing.py
@@ -0,0 +1,14 @@
+import sys
+import os
+#temporarily add the 
+#sys.path.append(os.getcwd)
+#import Common
+from ...DataStructures.LL import LinkedList
+
+
+def main():
+	print('test')
+	print(os.getcwd())
+
+if __name__ == '__main__':
+	main()
